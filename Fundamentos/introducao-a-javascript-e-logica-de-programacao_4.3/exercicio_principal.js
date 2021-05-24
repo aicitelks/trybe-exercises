93761fe0790a40a0f2be8d68ad5f25b94b7ee5f2
Letícia Castro <aicitelks@gmail.com>
2021-05-23 21:10:41 -0300

    Exercicio 3 finalizado com sucesso. Triangulo invertido

--- Fundamentos/introducao-a-javascript-e-logica-de-programacao_4.3/exercicio_principal.js
+++ Fundamentos/introducao-a-javascript-e-logica-de-programacao_4.3/exercicio_principal.js
@@ -1,4 +1,4 @@
-/*
+/*  EXERCÍCIO 1
     1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo: 
     
     n = 5
@@ -29,7 +29,7 @@ for (let index = 1; index <= n; index += 1) {
 };
 
 
-/*
+/*  EXERCÍCIO 2
     Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo: 
     n = 5
 
@@ -47,4 +47,36 @@ console.log('Exercício 2');
 for (let index = 1; index <= n; index += 1) {
     linha2 = linha2 + asterisco;
     console.log(linha2);
+}
+
+/*  EXERCÍCIO 3
+    3- Agora inverta o lado do triângulo. Por exemplo: 
+    n = 5
+
+    *
+   **
+  ***
+ ****
+*****
+Atenção! Note que esse exercício é bem mais complexo que o anterior! 
+Não basta, aqui, imprimir somente asteriscos. Você precisará de uma lógica para imprimir espaços também. 
+*/
+
+// Foi consultado o gabarito para o desenvolvimento do  exercício 3!
+let linha3 = '';
+let posicao = n; // inputPosition
+
+console.log('Exercício 3');
+
+for (let index = 0; index < n; index += 1) {
+    for (let index2 = 0; index2 <= n; index2 += 1) {
+        if (index2 < posicao) {
+            linha3 = linha3 + ' ';
+        } else {
+            linha3 = linha3 + asterisco;
+        }
+    }
+    console.log(linha3);
+    linha3 = '';
+    posicao -= 1;
 }
