ed5dfc11aa550201ec543e003edcd24a3f495aaa
Letícia Castro <aicitelks@gmail.com>
2021-05-23 21:12:00 -0300

    Exercicio 6 finalizado com sucesso (bonus). Numeros primos.

---
+++ Fundamentos/introducao-a-javascript-e-logica-de-programacao_4.3/exercicio_bonus.js
@@ -0,0 +1,22 @@
+/*
+    6- Faça um programa que diz se um número definido numa variável é primo ou não.
+
+    Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, a divisão dele com quaisquer 
+    outros números dá resto diferente de zero.
+    
+    Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; 
+    Além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+*/
+
+let num = 67;
+let count = 0;
+for (i = 0; i <= num; i++) {
+    if ((num % i) === 0) {
+        count = count + 1;
+    };
+};
+    if (count > 2) {
+        console.log('O número: ' + num + ' NÃO é primo');
+    } else {
+        console.log('O número: ' + num + ' é primo');
+    };
