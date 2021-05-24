e2691f0ef199191f2d5438a722ab685fc54f0d50
Letícia Castro <aicitelks@gmail.com>
2021-05-23 21:45:39 -0300

    Atividade extra: Tabuada (melhorias) calculo 1 a 10)

--- Fundamentos/introducao-a-javascript-e-logica-de-programacao_4.3/tabuada.js
+++ Fundamentos/introducao-a-javascript-e-logica-de-programacao_4.3/tabuada.js
@@ -1,7 +1,17 @@
-let num = 8;
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let tabuada = [];
 
-console.log('TABUADA DO ' + num);
-console.log();
-for (i = 1; i <= 10; i++) {
-    console.log(num + ' x ' + i +' = ' + num * i);
-}
+console.log('TABUADA DO 1');
+
+//para percorrer o array numeros
+for (let i = 0; i < 10; i++) {
+    //para executar o cálculo
+    for (let t = 1; t <= 10; t++) {
+        tabuada[t] = numeros[i] * t;
+        console.log(numeros[i] + ' x ' + t + ' = ' + tabuada[t]);
+    };
+    if (i < 9) {
+        console.log('');
+        console.log('TABUADA DO ' + numeros[i+1]);
+    }
+};
