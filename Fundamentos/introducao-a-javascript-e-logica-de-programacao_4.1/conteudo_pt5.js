cbaa25c5c5c25d0a977d5e8f925bb192077e5505
Letícia Castro <aicitelks@gmail.com>
2021-05-27 23:48:54 -0300

    Diretorio renomeado e reorganizado

---
+++ Fundamentos/introducao-a-javascript-e-logica-de-programacao_4.1/conteudo_pt5.js
@@ -0,0 +1,18 @@
+let nota = 50;
+
+switch (nota) {
+    case 80:
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+
+    case 60:
+        console.log('Vocẽ está na nossa lista de espera!');
+        break
+
+    case 50:
+        console.log('Você foi reprovada(o)!');
+        break;
+
+    default:
+        console.log('Não se aplica');
+}
