630cca4cdbd99f2361e8db19e4d3ef02da4a6486
Letícia Castro <aicitelks@gmail.com>
2021-06-01 00:40:48 -0300

    Deletado diretório bloco-4/dia-2. Arquivos movidos (introducao-a-javascript-e-logica-de-programacao_4.2)

--- Fundamentos/bloco-4/dia-2/forOf.js
+++
@@ -1,7 +0,0 @@
-// Exercício 1 Utilize o for/of para imprimir os elementos da lista names com o console.log() :
-
-let names = ['Huguinho', 'Zézinho', 'Luizinho', 'Patrícia'];
-
-for (let nome of names) {
-    console.log(nome);
-}
