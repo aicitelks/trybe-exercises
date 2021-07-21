d29903f097aeea28ec67dc9080fe1c5519764cc9
Letícia Castro <aicitelks@gmail.com>
2021-07-21 13:26:53 -0300

    Primeiro React Component criado com sucesso :rocket:

---
+++ FrontEnd/first-component/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import PrimeiroComponent from './Component';
+
+function App() {
+  return <PrimeiroComponent />;
+}
+
+export default App;
---
+++ FrontEnd/first-component/src/Component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PrimeiroComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World! </h1>
+        <p> I am a first React Class Component </p>
+        <p> 🎉 \(o.~)/ 🎉 </p>
+      </div>
+    )
+  }
+}
+
+export default PrimeiroComponent;
