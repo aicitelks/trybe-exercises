b49a0da3a92b32090cac69e55e41f9e9e4cce6d6
Letícia Castro <aicitelks@gmail.com>
2021-07-21 21:52:39 -0300

    Index.js principal atualizado

--- FrontEnd/introducao-a-react_11.2/fixation-exercises-11-2/src/index.js
+++ FrontEnd/introducao-a-react_11.2/fixation-exercises-11-2/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import App from './App2';
+//import App from './App2';
+//import App from './App3';
+import App from './App4';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
