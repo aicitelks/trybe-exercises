6b88d1d1f85fb33b087d24fdcce314292b9c68a3
Letícia Castro <aicitelks@gmail.com>
2021-08-07 20:20:49 -0300

    Exercicio de fixaçao 2 finalizado

--- FrontEnd/componentes-com-estado-eventos-e-formularios-com-react_12.1/fancy-buttons/src/App.js
+++ FrontEnd/componentes-com-estado-eventos-e-formularios-com-react_12.1/fancy-buttons/src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
 
+    // o this sendo vinculado manulamente para a função poder usar esse objeto dentro dela
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  // uma arrow function não precisa ter o bind no constructor, porem ela deixa a app menos performática
   handleClick1 = () => {
     console.log('Você clicou no botão 1');
   }
@@ -12,11 +19,13 @@ class App extends React.Component {
     console.log('Você clicou no botão 2');
   }
 
-  handleClick3 = () => {
-    console.log('Você clicou no botão 3');
+  handleClick3() {
+    console.log('Abaixo o objeto this');
+    console.log(this);
   }
 
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -26,7 +35,7 @@ class App extends React.Component {
           </p>
           <button onClick={ this.handleClick1 }>Botão 1</button>
           <button onChange={ this.handleClick2 }>Botão 2</button>
-          <button onSubmit={ this.handleClick3 }>Botão 3</button>
+          <button onClick={ this.handleClick3 }>Chamar o this</button>
         </header>
       </div>
     );
