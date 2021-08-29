25056fa0f0203e39ea1d225ad9549bbcbc40765b
Letícia Castro <aicitelks@gmail.com>
2021-08-29 18:27:58 -0300

    [REDDIT] Criado <App />

--- FrontEnd/gerenciamento-de-estado-com-redux_16.4/reddit/src/App.js
+++ FrontEnd/gerenciamento-de-estado-com-redux_16.4/reddit/src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { selectSubreddit, fetchPostsIfNeeded, refreshSubreddit } from './actions';
+import Posts from './components/Posts';
+import Selector from './components/Selector';
+
+class App extends React.Component {
+  componentDidMount() {
+    const { dispatch, selectSubreddit } = this.props;
+    dispatch(fetchPostsIfNeeded(selectSubreddit));
+  }
+
+  componentDidUpdate(prevProps) {
+    const { props } = this;
+
+    if (prevProps.selectSubreddit !== props.selectSubreddit) {
+      const { dispatch, selectSubreddit } = props;
+      dispatch(fetchPostsIfNeeded(selectSubreddit));
+    }
+  }
+
+  selectSubreddit(nextSubreddit) {
+    const { dispatch } = this.props;
+    dispatch(selectSubreddit(nextSubreddit));
+  }
+
+  handleRefreshClick(event) {
+    event.preventDefalt();
+
+    const { dispatch, selectSubreddit } = this.props;
+    dispatch(refreshSubreddit(selectSubreddit));
+    dispatch(fetchPostsIfNeeded(selectSubreddit));
+  }
+
+  renderLastUpdateAt() {
+    const { lastUpdated } = this.props;
+
+    return (
+      <span>
+        {`Last update at: ${new Date(lastUpdated).toLocaleTimeString()}.`}
+      </span>
+    );  
+  }
+  
+  renderRefreshButton() {
+    const { isFetching } = this.props;
+
+    return (
+      <button
+        type="button"
+        onClick={ (event) => this.handleRefreshClick(event) }
+        disabled={ isFetching }
+      >
+        Refresh
+      </button>
+    );
+  }
+
+  render() {
+    const {
+      availableSubreddits,
+      selectedSubreddit,
+      posts = [],
+      isFetching,
+      lastUpdated,
+    } = this.props;
+
+    const isEmpty = posts.length === 0;
+
+    return (
+      <div>
+        <Selector
+          value={ selectSubreddit }
+          onChange={ (nextSubreddit) => this.selectSubreddit(nextSubreddit) }
+          options={ availableSubreddits }
+        />
+
+        <div>
+          { lastUpdated && this.renderLastUpdateAt() }
+          { this.renderRefreshButton() }
+        </div>
+        { isFetching && <h2>Loading...</h2> }
+        { !isFetching && isEmpty && <h2>Não há posts</h2> }
+        { !isFetching && !isEmpty && <Posts posts={posts} /> }
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { selectSubreddit, postsBySubreddit } = state;
+  const {
+    isFetching,
+    lastUpdated,
+    items: posts,
+  } = postsBySubreddit[selectSubreddit];
+
+  return {
+    selectSubreddit,
+    posts,
+    isFetching,
+    lastUpdated,
+    availableSubreddits: Object.keys(postsBySubreddit),
+  };
+};
+
+App.propTypes = {
+  availableSubreddits: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  dispatch: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  lastUpdated: PropTypes.number,
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  })),
+  selectedSubreddit: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  lastUpdated: null,
+  posts: [],
+};
+
+export default connect(mapStateToProps)(App);
