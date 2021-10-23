69edccfdc670496c3ad60ddae309d0fbbcc13dac
Letícia Castro <aicitelks@gmail.com>
2021-10-23 15:22:15 -0300

    Exercícios do conteudo - Triggers - exe3 concluído

--- BackEnd/funcoes_SQL_JOINS_subqueries_20.3/exercicios_triggers(conteudo).sql
+++ BackEnd/funcoes_SQL_JOINS_subqueries_20.3/exercicios_triggers(conteudo).sql
@@ -35,4 +35,17 @@ DELIMITER ;
 
 UPDATE carros SET preco = 100.00 WHERE id_carro = 3;
 
--- 3) Crie um TRIGGER que, a cada exclusão feita na tabela carros , envie para a tabela log_operacoes as informações do tipo_operacao como 'EXCLUSÃO' e a data_ocorrido como o momento da operação.
+-- 3) Crie um TRIGGER que, a cada exclusão feita na tabela carros ,
+-- envie para a tabela log_operacoes as informações do 
+-- tipo_operacao como 'EXCLUSÃO' e a data_ocorrido como o momento da operação.
+DELIMITER $$
+CREATE TRIGGER delete_carros
+	AFTER DELETE ON carros
+    FOR EACH ROW
+BEGIN
+	INSERT INTO log_operacoes (tipo_operacao, data_ocorrido) VALUES ('EXCLUSÃO', NOW());
+END $$
+DELIMITER ;
+
+DELETE FROM carros WHERE id_carro = 2;
+SELECT * FROM log_operacoes;
