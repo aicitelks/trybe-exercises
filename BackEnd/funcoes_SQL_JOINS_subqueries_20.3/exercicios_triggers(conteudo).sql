8ddc86c1df5a1d07da3ac32902dd4c41af3165ba
Letícia Castro <aicitelks@gmail.com>
2021-10-23 15:18:13 -0300

    Exercícios do conteudo - Triggers - exe2 concluído

--- BackEnd/funcoes_SQL_JOINS_subqueries_20.3/exercicios_triggers(conteudo).sql
+++ BackEnd/funcoes_SQL_JOINS_subqueries_20.3/exercicios_triggers(conteudo).sql
@@ -17,9 +17,22 @@ INSERT INTO carros (preco) VALUES (30.000);
 SELECT * FROM carros;
 
 DROP TRIGGER insert_carros;
--- Error Code: 1442. Can't update table 'carros' in stored function/trigger 
--- because it is already used by statement which invoked this stored function/trigger.
 
--- 2) Crie um TRIGGER que, a cada atualização feita na tabela carros , defina o valor da coluna data_atualizacao para o momento do ocorrido e a acao para 'ATUALIZAÇÃO' .
+
+-- 2) Crie um TRIGGER que, a cada atualização feita na tabela carros ,
+--  defina o valor da coluna data_atualizacao para o momento do ocorrido e a acao para 'ATUALIZAÇÃO' .
+
+DELIMITER $$
+CREATE TRIGGER update_carros
+	BEFORE UPDATE ON carros
+    FOR EACH ROW
+BEGIN
+	SET
+		NEW.data_atualizacao = NOW(),
+        NEW.acao = 'ATUALIZAÇÃO';
+END $$
+DELIMITER ;
+
+UPDATE carros SET preco = 100.00 WHERE id_carro = 3;
 
 -- 3) Crie um TRIGGER que, a cada exclusão feita na tabela carros , envie para a tabela log_operacoes as informações do tipo_operacao como 'EXCLUSÃO' e a data_ocorrido como o momento da operação.
