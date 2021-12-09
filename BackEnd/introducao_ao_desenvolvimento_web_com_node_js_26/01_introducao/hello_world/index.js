a93823af6bf285eabe09a3715adcbe88cda857dd
Letícia Castro <aicitelks@gmail.com>
2021-12-09 08:29:18 -0300

    exercicio de conteudo, hello-world |  | EXERCICIO FINALIZADO

--- BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/01_introducao/hello_world/index.js
+++ BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/01_introducao/hello_world/index.js
@@ -1,2 +1,9 @@
+const readline = require('readline-sync');
+
 console.log('Hello World!');
 console.log('Deus seja louvado. Amém!');
+
+const name = readline.question('Qual o seu nome? ');
+const age = readline.questionInt('E a sua idade? ');
+
+console.log(`${name} Seja bem-vinda! E parabéns por seus ${age} anos de idade. Saúde!`);
