e93d9e0f0dd12249580add3f9498cddc7f164f1f
Letícia Castro <aicitelks@gmail.com>
2021-12-09 19:02:29 -0300

    03_testes_com_node | Exemplo 2, implementação no test do chai

--- BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/03_teste_com_node_Exemplo2/io-test/test.js
+++ BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/03_teste_com_node_Exemplo2/io-test/test.js
@@ -1,12 +1,18 @@
+const leArquivo = require('./leArquivo');
+
+const CONTEUDO_DO_ARQUIVO = 'VQV com TDD';
+
 describe('leArquivo', () => {
   describe('Quando o arquivo existe', () => {
     describe('a resposta', () => {
+      const resposta = leArquivo('arquivo.txt');
+
       it('é uma string', () => {
-        //
+        expect(resposta).to.be.a('string'); // 'a' valida o TIPO do retorno
       });
 
       it('é igual ao conteúdo do arquivo', () => {
-        //
+        expect(resposta).to.be.equals(CONTEUDO_DO_ARQUIVO);
       });
     });
   });
@@ -14,7 +20,9 @@ describe('leArquivo', () => {
   describe('Quando o arquivo não existe', () => {
     describe('a resposta', () => {
       it('é igual a "null"', () => {
-        //
+        const resposta = leArquivo('arquivo_que_nao_existe.txt');
+
+        expect(resposta).to.be.equal(null);
       });
     });
   });
