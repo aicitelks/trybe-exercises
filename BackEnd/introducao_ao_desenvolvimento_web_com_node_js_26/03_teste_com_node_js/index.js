5c34f9610e67cb7d7b010a93ea8d3f14cb85509c
Letícia Castro <aicitelks@gmail.com>
2021-12-09 16:05:35 -0300

    03_testes_com_node

---
+++ BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/03_teste_com_node_js/index.js
@@ -0,0 +1,43 @@
+// TESTANDO OS CENÁRIOS MANUALMENTE
+
+const calculaSituacao = require('./funcoes/calculo/calculaSituacao');
+
+console.log('Quando a média for menor que 7, retorna "reprovado":');
+
+const respostaCenario1 = calculaSituacao(4);
+if (respostaCenario1 === 'reprovado') {
+  console.log(`Ok 🚀`);
+} else {
+  console.error('Resposta não esperada 🚨');
+}
+// console:
+// Quando a média for menor que 7, retorna "reprovado":
+// Ok 🚀
+
+const calculaSituacao = require('./funcoes/calculo/calculaSituacao');
+
+console.log('Quando a média for maior que 7, retorna "aprovado":');
+
+const respostaCenario2 = calculaSituacao(9);
+if (respostaCenario2 === 'aprovado') {
+  console.log(`Ok 🚀`);
+} else {
+  console.error('Resposta não esperada 🚨');
+}
+// console:
+// Quando a média for maior que 7, retorna "aprovado":
+// Ok 🚀
+
+const calculaSituacao = require('./funcoes/calculo/calculaSituacao');
+
+console.log('Quando a média for igual a 7, retorna "aprovado":');
+
+const respostaCenario3 = calculaSituacao(7);
+if (respostaCenario3 === 'aprovado') {
+  console.log(`Ok 🚀`);
+} else {
+  console.error('Resposta não esperada 🚨');
+}
+// console:
+// Quando a média for igual a 7, retorna "aprovado":
+// Resposta não esperada 🚨
