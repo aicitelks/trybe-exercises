03c696ffb69f1aecf7c9c72ed8c3572211732a24
Letícia Castro <aicitelks@gmail.com>
2021-12-12 08:55:05 -0300

    Implementação do sinon e testes passando com sucesso

--- BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/03_teste_com_node_Exemplo2/io-test/test.js
+++ BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/03_teste_com_node_Exemplo2/io-test/test.js
@@ -1,28 +1,50 @@
 const fs = require('fs');
+// Sinon cria um comportamento fake para a função readFileSync, assim não é necessário realizar nenhuma operação de IO nem a criação de um arquivo real (não existe o 'arquvio.txt' neste projeto)
 const sinon = require('sinon');
 const { expect } = require('chai');
+
 const leArquivo = require('./leArquivo');
 
 const CONTEUDO_DO_ARQUIVO = 'VQV com TDD';
 
-sinon.stub(fs, 'readFileSync').returns(CONTEUDO_DO_ARQUIVO);
-
 describe('leArquivo', () => {
   describe('Quando o arquivo existe', () => {
-    describe('a resposta', () => {
-      const resposta = leArquivo('arquivo.txt');
 
+    before(() => {
+      sinon.stub(fs, 'readFileSync').returns(CONTEUDO_DO_ARQUIVO);
+    });
+
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
+    describe('a resposta', () => {
       it('é uma string', () => {
-        expect(resposta).to.be.a('string'); // 'a' valida o TIPO do retorno
+        const resposta = leArquivo('arquivo.txt');
+
+        expect(resposta).to.be.a('string');
       });
 
       it('é igual ao conteúdo do arquivo', () => {
+        const resposta = leArquivo('arquivo.txt');
+
         expect(resposta).to.be.equals(CONTEUDO_DO_ARQUIVO);
       });
     });
   });
 
   describe('Quando o arquivo não existe', () => {
+
+    before(() => {
+      sinon
+        .stub(fs, 'readFileSync')
+        .throws(new Error('Arquivo não encontrado'));
+    });
+
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
     describe('a resposta', () => {
       it('é igual a "null"', () => {
         const resposta = leArquivo('arquivo_que_nao_existe.txt');
