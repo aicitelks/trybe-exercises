b6569ec0c832f42988ccf649ed2bc7380ba5fc95
Letícia Castro <aicitelks@gmail.com>
2021-12-20 09:53:56 -0300

    05_express_middlewares | Usando middlewares definidos em funções separadas

--- BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/05_express_middlewares/recipes/index.js
+++ BackEnd/introducao_ao_desenvolvimento_web_com_node_js_26/05_express_middlewares/recipes/index.js
@@ -10,6 +10,23 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+function validateName(req, res, next) {
+  const { name } = req.body;
+  // VALIDA SE O NAME ESTÁ VAZIO, E RETORNA PRA REQUISIÇÃO O STATUS 400 E O JSON COM A MENSAGEM
+  if (!name || name === '') return res.status(400).json({ message: 'O name não pode ser vazio!' }); // 1
+
+  // CHAMA O PRÓXIMO MIDDLEWARE
+  next(); // 2
+}
+
+function validatePrice(req, res, next){
+  const { price } = req.body;
+  if (typeof price != 'number' || price <= 0)
+    return res.status(400).json({ message: 'O preço deve ser numérico e/ou maior que 0' });
+
+  next();
+}
+
 app.get('/recipes', function (req, res) {
   res.status(200).json(recipes);
 });
@@ -27,14 +44,8 @@ app.get('/recipes/:id', function (req, res) {
 
   res.status(200).json(recipe);
 });
-/* 
-app.post('/recipes', function (req, res) {
-  const { id, name, price, waitTime } = req.body;
-  recipes.push({ id, name, price, waitTime });
-  res.status(201).json({ message: 'Recipe created successfully!' });
-});
- */
-app.put('/recipes/:id', function (req, res) {
+
+app.put('/recipes/:id', validateName, validatePrice, function (req, res) {
   const { id } = req.params;
   const { name, price, waitTime } = req.body;
   const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
@@ -57,19 +68,11 @@ app.delete('/recipes/:id', function (req, res) {
   res.status(204).end();
 });
 
-// ... PARTE 2 ...
-
 /** UMA ROTA, DOIS MIDDLEWARES */
-app.post('/recipes', 
+app.post('/recipes',
   // MIDDLEWARE 1
-  function (req, res, next) {
-    const { name } = req.body;
-    // VALIDA SE O NAME ESTÁ VAZIO, E RETORNA PRA REQUISIÇÃO O STATUS 400 E O JSON COM A MENSAGEM
-    if (!name || name === '') return res.status(400).json({ message: 'Invalid data!' }); // 1
-
-    // CHAMA O PRÓXIMO MIDDLEWARE
-    next(); // 2
-  },
+  validateName,
+  validatePrice,
   // MIDDLEWARE 2
   function (req, res) { // 3
     const { id, name, price } = req.body;
@@ -77,7 +80,6 @@ app.post('/recipes',
     res.status(201).json({ message: 'Recipe created successfully!' });
   }
 );
-// ...
 
 app.all('*', function (req, res) {
     return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
